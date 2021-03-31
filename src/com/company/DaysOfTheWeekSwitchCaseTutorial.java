9e102f1b3a48a0eaf6c5196305b143e95db6959f
lacabaRuel <ruel.lacabaph@gmail.com>
2021-03-31 22:10:11 +0800

    ADD CHANGES

--- src/com/company/DaysOfTheWeekSwitchCaseTutorial.java
+++ src/com/company/DaysOfTheWeekSwitchCaseTutorial.java
@@ -4,7 +4,9 @@ public class DaysOfTheWeekSwitchCaseTutorial {
 
     public static void main(String[] args) {
         printDaysOfTheWeek(8);
+        printNumberInWord(2);
     }
+
     public static void printDaysOfTheWeek(int day) {
         switch (day) {
             case 1:
@@ -35,4 +37,45 @@ public class DaysOfTheWeekSwitchCaseTutorial {
         }
     }
 
-}
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("INVALID NUMBER");
+                break;
+
+
+        }
+
+    }
+    }
+
---
+++ src/com/company/ForLoop.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("INCREMENT LOOP");
+
+        for(double i = 1 ; i<10 ; i++){
+            System.out.println("10,000 at " + i + " % interest " + computeInterest(10000 , i));
+        }
+
+        System.out.println("DECREMENT LOOP");
+        for(double i = 10 ; i!=0 ; i--){
+            System.out.println("10,000 at " + i + " % interest " + computeInterest(10000 , i));
+        }
+
+    }
+
+    public static double computeInterest(double amount ,double interestRate){
+        return (amount*(interestRate/100));
+    }
+}
